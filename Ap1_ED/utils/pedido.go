d441aeb3e5b4ac03220af04e9d48f07e04d2293f
Ana Beatriz Lessa <126483904+Bialogy@users.noreply.github.com>
2023-10-09 09:13:02 -0300

    Add files via upload

---
+++ Ap1_ED/utils/pedido.go
@@ -0,0 +1,24 @@
+package utils
+
+type Pedido struct {
+	Id       int
+	Delivery bool
+}
+
+var pedidos []Pedido
+
+func CalculaValorTotalPedido() {
+
+}
+
+func PostPedido(pedido Pedido) {
+
+}
+
+func DeletePedido(idPedido int) {
+
+}
+
+func FindPedidoById(idPedido int) *Pedido {
+
+}
